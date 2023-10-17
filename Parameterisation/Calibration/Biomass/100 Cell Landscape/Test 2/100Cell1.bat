5d365e2cf9375878aef8c42247b822b213020ee6
lagoodal <97845125+lagoodal@users.noreply.github.com>
2023-10-17 10:18:06 -0400

    Add files via upload

---
+++ Parameterisation/Calibration/Biomass/100 Cell Landscape/Test 2/100Cell1.bat
@@ -0,0 +1,12 @@
+rem Batch File to Run a Scenario 
+
+rem The 'rem' keyword indicates that this is a remark
+
+
+call landis-ii-7 100Cell1_scenario.txt
+
+
+rem call landis-ii scenario-single-cell.txt
+
+
+pause
